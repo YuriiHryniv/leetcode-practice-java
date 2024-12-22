200f24274d330a24daa33a167f23715e9122a545
Jorgen <superyura444@gmail.com>
2024-12-22 17:56:04 +0200

    word search

--- src/word_search/Solution.java
+++ src/word_search/Solution.java
@@ -4,72 +4,44 @@ import java.util.*;
 
 class Solution {
     public boolean exist(char[][] board, String word) {
-        Stack<Character> stringStack = new Stack<>();
-        boolean[][] visited = new boolean[board.length][board[0].length];
+        int m = board.length;
+        int n = board[0].length;
 
-        for (int row = 0; row < board.length; row++) {
-            for (int column = 0; column < board[0].length; column++) {
-                if (word.charAt(0) == board[row][column]) {
-                    boolean found = findWord(visited, board, word, row, column, stringStack);
-                    if (found) {
-                        return true;
-                    } else {
-                        stringStack.clear();
-                    }
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
                 }
             }
         }
-        return false;
 
+        return false;
     }
 
-    private boolean findWord(boolean[][] visited, char[][] board, String word, int row, int column, Stack<Character> stringStack) {
-        char found = board[row][column];
-        visited[row][column] = true;
-
-        if (stringStack.size() >= word.length()) {
-            return false;
-        }
-
-        boolean first = false;
-        boolean second = false;
-        boolean third = false;
-        boolean fourth = false;
-
-        if (found == word.charAt(stringStack.size())) {
-            stringStack.push(found);
-        } else {
-            visited[row][column] = false;
-            return false;
-        }
-
-        if (found == word.charAt(word.length() - 1) && stringStack.size() == word.length()) {
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
             return true;
         }
 
-        if (row - 1 >= 0 && !visited[row - 1][column]) {
-            first = findWord(visited, board, word, row - 1, column, stringStack);
-        }
-
-        if (row + 1 <= board.length - 1 && !visited[row + 1][column]) {
-            second = findWord(visited, board, word, row + 1, column, stringStack);
-        }
-
-        if (column - 1 >= 0 && !visited[row][column - 1]) {
-            third = findWord(visited, board, word, row, column - 1, stringStack);
-        }
-
-        if (column + 1 <= board[0].length - 1 && !visited[row][column + 1]) {
-            fourth = findWord(visited, board, word, row, column + 1, stringStack);
-        }
-
-        if (!(first || second || third || fourth)) {
-            stringStack.remove(stringStack.getLast());
-            visited[row][column] = false;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
-        } else {
+        }
+
+        visited[i][j] = true;
+
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+                backtrack(board, word, visited, i - 1, j, index + 1) ||
+                backtrack(board, word, visited, i, j + 1, index + 1) ||
+                backtrack(board, word, visited, i, j - 1, index + 1)) {
             return true;
         }
+
+        visited[i][j] = false;
+        return false;
     }
 
     public static void main(String[] args) {
