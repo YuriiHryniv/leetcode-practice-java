b1054e466804cf8132f685e79921317b1d5ebf34
Jorgen <superyura444@gmail.com>
2024-12-24 23:51:50 +0200

    word break, dp

--- src/word_break/Solution.java
+++ src/word_break/Solution.java
@@ -1,6 +1,5 @@
 package word_break;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,39 +7,24 @@ import java.util.Set;
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
-
-        int left = 0;
-        int right = left;
-        String copyRes = s.intern();
-
-        while (right <= s.length()) {
-            if (set.contains(s.substring(left, right)) && !isPartOfLargerWord(s.substring(left, right), wordDict, copyRes.length())) {
-                copyRes = s.substring(right);
-                left = right;
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                } else {
+                    dp[i] = false;
+                }
             }
-            right++;
-        }
 
-        return copyRes.isEmpty();
-    }
-
-    private boolean isPartOfLargerWord(String substring, List<String> wordDict, int targetLength) {
-        if (substring.length() == targetLength) {
-            return false;
         }
-        boolean isPart = false;
-
-        for (String word: wordDict) {
-            if (word.startsWith(substring) && !substring.equals(word) && word.length() <= targetLength) {
-                isPart = true;
-                break;
-            }
-        }
-        return isPart;
+        return dp[s.length()];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.wordBreak("aaaaaaa", List.of("aaaa","aaa")));
+        System.out.println(solution.wordBreak("leetcode", List.of("leet","code")));
     }
 }
