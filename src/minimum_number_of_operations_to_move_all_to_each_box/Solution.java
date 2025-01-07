4db7ad01977d5cd20dc9fc14c373d9ae08e06761
Jorgen <superyura444@gmail.com>
2025-01-07 19:24:07 +0200

    minimum_number_of_operations_to_move_all_to_each_box

--- src/minimum_number_of_operations_to_move_all_to_each_box/Solution.java
+++ src/minimum_number_of_operations_to_move_all_to_each_box/Solution.java
@@ -5,16 +5,34 @@ import java.util.Queue;
 
 class Solution {
     public int[] minOperations(String boxes) {
-        int[] res = new int[boxes.length()];
+        int[] left = new int[boxes.length()];
+        int[] right = new int[boxes.length()];
+
+        int leftNumberOfOnes = 0;
+        int totalSumLeft = 0;
+
         for (int i = 0; i < boxes.length(); i++) {
-            int sum = 0;
-            for (int j = 0; j < boxes.length(); j++) {
-                if (j == i) continue;
-                if (boxes.charAt(j) == '1') {
-                    sum += Math.abs(i - j);
-                }
+            totalSumLeft += leftNumberOfOnes;
+            left[i] = totalSumLeft;
+            if (boxes.charAt(i) == '1') {
+                leftNumberOfOnes++;
             }
-            res[i] = sum;
+        }
+
+        int rightNumberOfOnes = 0;
+        int totalSumRight = 0;
+
+        for (int i = right.length - 1; i >= 0; i--) {
+            totalSumRight += rightNumberOfOnes;
+            right[i] = totalSumRight;
+            if (boxes.charAt(i) == '1') {
+                rightNumberOfOnes++;
+            }
+        }
+
+        int[] res = new int[boxes.length()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = left[i] + right[i];
         }
         return res;
     }
