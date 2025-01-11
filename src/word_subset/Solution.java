862bd7a40495d43957e5642e5c6b91bdc309cdc0
Jorgen <superyura444@gmail.com>
2025-01-11 20:43:38 +0200

    word_subset

--- src/word_subset/Solution.java
+++ src/word_subset/Solution.java
@@ -4,72 +4,35 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    public List<String> wordSubsets(String[] words1, String[] words2) {
-        List<String> res = new ArrayList<>();
-        Map<Character, Integer> desiredFreq = new HashMap<>();
-        Map<Character,Integer> localFreq = new HashMap<>();
-        String desiredString = Arrays.stream(words2).collect(Collectors.joining(" "));;
-
-        for (int i = 0; i < desiredString.length(); i++) {
-            char c = desiredString.charAt(i);
-            if (c == ' ') {
-                merge(localFreq, desiredFreq);
-                localFreq.clear();
-                continue;
-            }
-            Integer value = localFreq.get(c);
-            if (value == null) {
-                localFreq.put(c, 1);
-            }else {
-                localFreq.put(c, ++value);
-            }
-        }
-        merge(localFreq, desiredFreq);
-        Map<Character, Integer> currentFreq = new HashMap<>();
-
-        for (String str: words1) {
-            currentFreq.clear();
-
-            for (int i = 0; i < str.length(); i++) {
-                Integer value = currentFreq.get(str.charAt(i));
-                if (value == null) {
-                    currentFreq.put(str.charAt(i), 1);
-                } else {
-                    currentFreq.put(str.charAt(i), ++value);
-                }
-            }
-
-            if (isSubset(currentFreq, desiredFreq)) {
-                res.add(str);
-            }
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] bmax = count("");
+        for (String b: B) {
+            int[] bCount = count(b);
+            for (int i = 0; i < 26; ++i)
+                bmax[i] = Math.max(bmax[i], bCount[i]);
         }
 
-        return res;
+        List<String> ans = new ArrayList();
+        search: for (String a: A) {
+            int[] aCount = count(a);
+            for (int i = 0; i < 26; ++i)
+                if (aCount[i] < bmax[i])
+                    continue search;
+            ans.add(a);
+        }
+
+        return ans;
     }
 
-    private void merge(Map<Character, Integer> local, Map<Character, Integer> desiredFreq) {
-        for (Map.Entry<Character, Integer> entry : local.entrySet()) {
-            Integer value = desiredFreq.get(entry.getKey());
-            if (value == null) {
-                desiredFreq.put(entry.getKey(), entry.getValue());
-            } else if (value < entry.getValue()) {
-                desiredFreq.put(entry.getKey(), entry.getValue());
-            }
-        }
-    }
-
-    private boolean isSubset(Map<Character, Integer> currentFreq, Map<Character, Integer> desiredFreq) {
-        for (Map.Entry<Character, Integer> entry: desiredFreq.entrySet()) {
-            Integer periodicity = currentFreq.get(entry.getKey());
-            if (periodicity == null || periodicity < entry.getValue()) {
-                return false;
-            }
-        }
-        return true;
+    public int[] count(String S) {
+        int[] ans = new int[26];
+        for (char c: S.toCharArray())
+            ans[c - 'a']++;
+        return ans;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.wordSubsets(new String[]{"amazon","apple","facebook","google","leetcode"}, new String[]{"lo","eeo", "oo"}));
+        System.out.println(solution.wordSubsets(new String[]{"amazon","apple","facebook","google","leetcode"}, new String[]{"e","o"}));
     }
 }
