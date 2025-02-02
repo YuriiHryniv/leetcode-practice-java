87f9984bbc1a3faf3255fa4f5ff181ab9480703e
Jorgen <superyura444@gmail.com>
2025-02-02 16:12:39 +0200

    check_if_array_is_sorted_and_rotated

--- src/check_if_array_is_sorted_and_rotated/Solution.java
+++ src/check_if_array_is_sorted_and_rotated/Solution.java
@@ -1,37 +1,33 @@
 package check_if_array_is_sorted_and_rotated;
 
-
 import java.util.Arrays;
 
 class Solution {
     public boolean check(int[] nums) {
         int[] copy = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            copy[i] = nums[i];
-        }
+        System.arraycopy(nums, 0, copy, 0, nums.length);
         Arrays.sort(copy);
-        for (int i = 0; i < nums.length; i++) {
-            if (Arrays.equals(rotate(nums, i), copy)) {
-                System.out.println(i);
-                return true;
-            }
+
+        int smallest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) smallest = i + 1;
         }
-        return false;
+        return Arrays.equals(rotate(nums, smallest), copy);
+
     }
 
     private int[] rotate(int[] nums, int k) {
         int[] rotated = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            rotated[(i + k) % nums.length] = nums[i];
-        }
+        System.arraycopy(nums, 0, rotated, nums.length - k, k);
+        System.arraycopy(nums, k, rotated, 0, nums.length - k);
 
         return rotated;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.check(new int[] {1,2,3}));
-
+        System.out.println(solution.check(new int[] {5,5,6,6,6,9,1,2}));
     }
 }
