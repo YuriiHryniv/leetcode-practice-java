b1c980c447665123fc8e868ec52e5d6972204135
Jorgen <superyura444@gmail.com>
2025-02-06 22:06:18 +0200

    tupleSameProduct

--- src/tuple_of_the_same_product/Solution.java
+++ src/tuple_of_the_same_product/Solution.java
@@ -5,30 +5,18 @@ import java.util.*;
 
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        Map<Integer, List<int[]>> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length - 1; i++) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (i == j) continue;
-                int key = nums[i] * nums[j];
-
-                List<int[]> currentKey = map.get(key);
-                if (currentKey == null) {
-                    List<int[]> list = new ArrayList<>();
-                    list.add(new int[] {nums[i], nums[j]});
-                    map.put(key, list);
-                } else {
-                    currentKey.add(new int[] {nums[i], nums[j]});
-                    map.put(key, currentKey);
-                }
+                int product = nums[i] * nums[j];
+                freq.put(product, freq.getOrDefault(product, 0) + 1);
             }
         }
 
         int combinationSum = 0;
 
-        for (Map.Entry<Integer, List<int[]>> entry : map.entrySet()) {
-            List<int[]> value = entry.getValue();
-            int size = value.size();
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int size = entry.getValue();
             combinationSum += (size * 2) * (2 * (size - 1));
         }
         return combinationSum;
