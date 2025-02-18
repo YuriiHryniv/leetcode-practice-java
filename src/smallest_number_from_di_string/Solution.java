cbdc028f257c6d8003a38dcce7ecf235e7eb8e12
Jorgen <superyura444@gmail.com>
2025-02-18 20:05:32 +0200

    smallest_number_from_di_string

--- src/smallest_number_from_di_string/Solution.java
+++ src/smallest_number_from_di_string/Solution.java
@@ -5,47 +5,20 @@ import java.util.*;
 class Solution {
 
     public String smallestNumber(String pattern) {
-        int number = pattern.length() + 1;
-        String allNumbers = "123456789";
-        pattern = pattern + "I";
-        allNumbers = allNumbers.substring(0, number);
+        int n = pattern.length();
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
 
-        int currentSign = 0;
-        PriorityQueue<String> pq = new PriorityQueue<String>((first, second) -> Integer.compare(Integer.parseInt(first), Integer.parseInt(second)));
-        dfs(allNumbers, "", currentSign, pattern, pq);
+        for (int i = 0; i <= n; i++) {
+            stack.push(i + 1);  // Push digits 1 to n+1
 
-        return pq.poll();
-    }
-
-    private static void dfs(String allNumbers, String res, int currentSign, String pattern, PriorityQueue<String> pq) {
-        if (res.length() == pattern.length()) {
-            pq.offer(res);
-            return;
-        }
-        for (int i = 0; i < allNumbers.length(); i++) {
-
-            char currentElement = allNumbers.charAt(i);
-
-            if (currentSign != 0 && pattern.charAt(currentSign - 1) == 'I') {
-                if (!res.isEmpty() && Character.getNumericValue(res.charAt(res.length() - 1)) > Character.getNumericValue(currentElement)) {
-                    return;
+            if (i == n || pattern.charAt(i) == 'I') {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
                 }
             }
-
-            if (currentSign != 0 && pattern.charAt(currentSign - 1) == 'D') {
-                if (!res.isEmpty() && Character.getNumericValue(res.charAt(res.length() - 1)) < Character.getNumericValue(currentElement)) {
-                    return;
-                }
-            }
-
-            String left = allNumbers.substring(0, i);
-            String right = allNumbers.substring(i + 1);
-
-            dfs(left + right, res + currentElement, ++currentSign, pattern, pq);
-
-            --currentSign;
-
         }
+        return result.toString();
     }
 
     public static void main(String[] args) {
